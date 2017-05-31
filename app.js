c7eea7b207b52a081614eccc9a475be53cc468d7
Karthikeyan1993 <karthik.infotec99@gmail.com>
2017-05-31 09:37:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by USER on 25/05/2017.
+ */
+$(document).ready(function () {
+    /**
+     * dynamic hyperlink function
+     */
+    $("#btnId").click(function () {
+        $("#urlID").attr('href', "https://acadgild.com/").text("ACADGLID");
+    });
+});
